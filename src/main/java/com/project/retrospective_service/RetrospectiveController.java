12dfabbfd86cdd36a5f8252bfe9866a58b68676f
Priyanka M R <priyankamr@Priyankas-MacBook-Air.local>
2023-10-24 15:27:06 +0800

    API changes

--- src/main/java/com/project/retrospective_service/RetrospectiveController.java
+++ src/main/java/com/project/retrospective_service/RetrospectiveController.java
@@ -9,7 +9,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/retrospectives")
@@ -20,44 +23,61 @@ public class RetrospectiveController {
 
     // Create a new retrospective
     @PostMapping
-    public ResponseEntity<String> createRetrospective( @RequestBody Retrospective retrospective) {
+    public ResponseEntity<String> createRetrospective(@RequestBody Retrospective retrospective) {
         logger.info("Creating a new retrospective: " + retrospective.getName());
         String inputDate = retrospective.getDate();
         List<String> inputParticipants = retrospective.getParticipants();
         List<Feedback> inputFeedback = retrospective.getFeedback();
-        if ((inputDate == null || inputParticipants.isEmpty()) && inputFeedback.isEmpty()) {
+        if (inputDate == null || inputParticipants.isEmpty()) {
             return new ResponseEntity<>("Date and Participants cannot be empty", HttpStatus.BAD_REQUEST);
+        }
+        if (!inputFeedback.isEmpty()) {
+            return new ResponseEntity<>("Feedback should be empty", HttpStatus.BAD_REQUEST);
         }
         retrospectives.add(retrospective);
         return new ResponseEntity<>("Retrospective created successfully", HttpStatus.CREATED);
     }
 
     // Add feedback item to a retrospective
-    @PostMapping("/{retrospectiveId}/feedback")
-    public ResponseEntity<String> addFeedback(@PathVariable int retrospectiveId, @RequestBody Feedback feedback) {
-        if (retrospectiveId >= 0 && retrospectiveId < retrospectives.size()) {
-            Retrospective retrospective = retrospectives.get(retrospectiveId);
-            retrospective.getFeedback().add(feedback);
-            logger.info("Add new feedback: " + retrospective.getFeedback());
+    @PostMapping("/{retrospectiveName}/feedback")
+    public ResponseEntity<String> addFeedback(@PathVariable String retrospectiveName, @RequestBody Feedback feedback) {
+        Optional<Retrospective> foundRetrospective = retrospectives.stream()
+                .filter(r -> r.getName().equals(retrospectiveName))
+                .findFirst();
+
+        if (foundRetrospective.isPresent()) {
+            foundRetrospective.get().getFeedback().add(feedback);
             return new ResponseEntity<>("Feedback added successfully", HttpStatus.CREATED);
         }
         return new ResponseEntity<>("Retrospective not found", HttpStatus.NOT_FOUND);
     }
 
     // Update feedback item
-    @PutMapping("/{retrospectiveId}/feedback/{feedbackId}")
-    public ResponseEntity<String> updateFeedback(@PathVariable int retrospectiveId, @PathVariable int feedbackId, @RequestBody Feedback feedback) {
-        if (retrospectiveId >= 0 && retrospectiveId < retrospectives.size()) {
-            Retrospective retrospective = retrospectives.get(retrospectiveId);
-            if (feedbackId >= 0 && feedbackId < retrospective.getFeedback().size()) {
-                Feedback existingFeedback = retrospective.getFeedback().get(feedbackId);
-                existingFeedback.setBody(feedback.getBody());
-                existingFeedback.setFeedbackType(feedback.getFeedbackType());
-                return new ResponseEntity<>("Feedback updated successfully", HttpStatus.OK);
-            }
+    @PutMapping("/{retrospectiveName}/feedback/{feedbackName}")
+    public ResponseEntity<String> updateFeedback(@PathVariable String retrospectiveName, @PathVariable String feedbackName, @RequestBody Feedback feedback) {
+        Retrospective retrospectiveToUpdate = retrospectives.stream()
+                .filter(r -> r.getName().equals(retrospectiveName))
+                .findFirst()
+                .orElse(null);
+
+        if (retrospectiveToUpdate == null) {
+            return new ResponseEntity<>("Retrospective not found", HttpStatus.NOT_FOUND);
+        }
+
+        Feedback feedbackToUpdate = retrospectiveToUpdate.getFeedback().stream()
+                .filter(f -> f.getName().equals(feedbackName))
+                .findFirst()
+                .orElse(null);
+
+        if (feedbackToUpdate == null) {
             return new ResponseEntity<>("Feedback not found", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>("Retrospective not found", HttpStatus.NOT_FOUND);
+
+        feedbackToUpdate.setBody(feedback.getBody());
+        feedbackToUpdate.setFeedbackType(feedback.getFeedbackType());
+
+        return new ResponseEntity<>("Feedback updated successfully", HttpStatus.OK);
+
     }
 
     // Get all retrospectives
@@ -66,6 +86,44 @@ public class RetrospectiveController {
         return retrospectives;
     }
 
+    //Get all retrospectives with Pagination
+    @GetMapping("/search")
+    public ResponseEntity<List<Retrospective>> getRetrospectivesWithPagination(
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @RequestParam(name = "pageSize", defaultValue = "2") int pageSize) {
+        return getListResponseEntity(page, pageSize, retrospectives);
+    }
+
+    private ResponseEntity<List<Retrospective>> getListResponseEntity(@RequestParam(name = "page", defaultValue = "1") int page, @RequestParam(name = "pageSize", defaultValue = "2") int pageSize, List<Retrospective> retrospectives) {
+        int startIndex = (page - 1) * pageSize;
+        if (startIndex >= retrospectives.size()) {
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
+        }
+
+        int endIndex = Math.min(startIndex + pageSize, retrospectives.size());
+        List<Retrospective> retrospectivesPage = retrospectives.subList(startIndex, endIndex);
+
+        return new ResponseEntity<>(retrospectivesPage, HttpStatus.OK);
+    }
+
+    //Search Retrospectives by Date
+    @GetMapping("/searchByDate")
+    public ResponseEntity<List<Retrospective>> searchRetrospectivesByDateWithPagination(
+            @RequestParam(name = "date") String date,
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @RequestParam(name = "pageSize", defaultValue = "2") int pageSize) {
+        List<Retrospective> foundRetrospectives = retrospectives.stream()
+                .filter(retrospective -> date.equals(retrospective.getDate()))
+                .collect(Collectors.toList());
+
+        if (foundRetrospectives.isEmpty()) {
+            logger.info("Retrospective list is empty");
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NOT_FOUND);
+        }
+
+        return getListResponseEntity(page, pageSize, foundRetrospectives);
+    }
+    
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
